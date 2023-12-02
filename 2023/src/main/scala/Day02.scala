809b6f3b7db2162a0e50ec6e156d4dfb2097fbf5
Laurin Novak <laurin@pellervoinen.com>
2023-12-02 13:03:11 +0200

    add solutions for day 1 part 2

--- 2023/src/main/scala/Day02.scala
+++ 2023/src/main/scala/Day02.scala
@@ -4,9 +4,9 @@ object Day02:
     private val input = FileReader.readFile("day02.txt")
 
     enum Cubes(val amount: Int, val max: Int):
-        case blue(a: Int)  extends Cubes(a, 14)
-        case red(a: Int)   extends Cubes(a, 12)
-        case green(a: Int) extends Cubes(a, 13)
+        case Blue(a: Int)  extends Cubes(a, 14)
+        case Red(a: Int)   extends Cubes(a, 12)
+        case Green(a: Int) extends Cubes(a, 13)
 
         def isValid: Boolean = amount <= max
     end Cubes
@@ -14,9 +14,9 @@ object Day02:
     object Cubes:
         private def fromColorString(color: String, amount: Int) =
             color match
-                case "blue"  => Cubes.blue(amount).some
-                case "red"   => Cubes.red(amount).some
-                case "green" => Cubes.green(amount).some
+                case "blue"  => Cubes.Blue(amount).some
+                case "red"   => Cubes.Red(amount).some
+                case "green" => Cubes.Green(amount).some
                 case _       => none
         end fromColorString
 
@@ -35,9 +35,9 @@ object Day02:
 
     case class Round(cubes: Seq[Cubes])
     object Round:
-        def fromString(i: String): Option[Round] =
+        def fromString(i: String, enableMax: Boolean): Option[Round] =
             val res     = i.trim.split(',').toSeq.flatMap(Cubes.fromString)
-            val isValid = res.forall(_.isValid)
+            val isValid = !enableMax || res.forall(_.isValid)
             Option.when(isValid)(Round(res))
         end fromString
 
@@ -45,29 +45,39 @@ object Day02:
 
     case class Game(id: Int, rounds: Seq[Round])
     object Game:
-        def fromString(i: String): Option[Game] =
+        def fromString(i: String, enableMax: Boolean): Option[Game] =
             val split = i.split(':').toSeq
             for
                 h      <- split.headOption
                 id     <- h.split(' ').lastOption
                 iid    <- id.toIntOption
                 l      <- split.lastOption
-                rounds <- l.split(';').toSeq.traverse(Round.fromString)
+                rounds <- l.split(';').toSeq.traverse(Round.fromString(_, enableMax))
             yield Game(iid, rounds)
 
         end fromString
     end Game
 
+    private def cubesNeeded(game: Game) =
+        game.rounds
+            .flatMap(_.cubes)
+            .groupMapReduce(_.ordinal)(_.amount)((a, b) => math.max(a, b))
+            .values
+            .product
+
     private def part1() =
-        val res = input.flatMap(Game.fromString)
+        val res = input.flatMap(Game.fromString(_, true))
 
         res.map(_.id).sum
     end part1
 
+    private def part2() =
+        input.flatMap(Game.fromString(_, false)).map(cubesNeeded).sum
+
     def run(): Unit =
         val res =
             s"""Part 1: ${part1()}
-               |""".stripMargin
+               |Part 2: ${part2()}""".stripMargin
         println(res)
 
 end Day02
